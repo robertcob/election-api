bd2e8cbab07b5cf4f069d4e513ab9540183911df
robrien <robert.obrien@yahooinc.com>
2023-10-23 19:56:01 +0200

    comment func

--- internal/poll/api.go
+++ internal/poll/api.go
@@ -1,20 +1,14 @@
 package poll
 
-import routing "github.com/go-ozzo/ozzo-routing/v2"
+import (
+	routing "github.com/go-ozzo/ozzo-routing/v2"
+	"github.com/qiangxue/go-rest-api/pkg/log"
+)
 
 // RegisterHandlers sets up the routing of the HTTP handlers.
 func RegisterHandlers(r *routing.RouteGroup, service Service, authHandler routing.Handler, logger log.Logger) {
-	res := resource{service, logger}
-
-	r.Get("/albums/<id>", res.get)
-	r.Get("/albums", res.query)
-
-	r.Use(authHandler)
-
-	// the following endpoints require a valid JWT
-	r.Post("/albums", res.create)
-	r.Put("/albums/<id>", res.update)
-	r.Delete("/albums/<id>", res.delete)
+	//res := resource{service,
+	// register endpoints related to albums here
 }
 
 type resource struct {
